18e1b34055cf7ebda06dba2aea9a28d0aefe231d
k-hara <k.hara.pg@gmail.com>
2024-08-14 14:55:24 +0900

    feat: FlexID画面にOIRのS係数を計算する機能タブを追加

---
+++ FlexID/Views/ScoeffCalcView.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace FlexID.Views
+{
+    /// <summary>
+    /// Interaction logic for ScoeffCalc
+    /// </summary>
+    public partial class ScoeffCalcView : UserControl
+    {
+        public ScoeffCalcView()
+        {
+            InitializeComponent();
+        }
+    }
+}
