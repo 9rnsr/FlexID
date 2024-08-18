ac3d45b24ae6b2c5cb714168c26adef7be8a2dee
k-hara <k.hara.pg@gmail.com>
2024-08-18 16:02:26 +0900

    feat: 線源領域OtherのS係数計算処理で読み込んだNDXファイルの情報を使用する

--- FlexID.Calc/LinqExtension.cs
+++ FlexID.Calc/LinqExtension.cs
@@ -8,7 +8,34 @@ namespace System.Linq
 
         public static IEnumerable<(T1, T2)> Zip<T1, T2>(this IEnumerable<T1> source1, IEnumerable<T2> source2)
         {
+            if (source1 is null)
+                throw new ArgumentNullException(nameof(source1));
+            if (source2 is null)
+                throw new ArgumentNullException(nameof(source2));
+
             return source1.Zip(source2, ZipSelector2);
+        }
+
+        public static IEnumerable<T> Prepend<T>(this IEnumerable<T> source, T element)
+        {
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+
+            yield return element;
+
+            foreach (var item in source)
+                yield return item;
+        }
+
+        public static IEnumerable<T> Append<T>(this IEnumerable<T> source, T element)
+        {
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+
+            foreach (var item in source)
+                yield return item;
+
+            yield return element;
         }
     }
 }
