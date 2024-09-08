f4e58f4360b355f5a6cfa02579ecbb0c9cecd8c8
k-hara <k.hara.pg@gmail.com>
2024-09-08 17:23:29 +0900

    fix: 人体モデル図と部位の凡例の部分をそれぞれUserControlとして切り出す

---
+++ FlexID.Viewer/Views/HumanContour.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace FlexID.Viewer.Views
+{
+    /// <summary>
+    /// HumanContour.xaml の相互作用ロジック
+    /// </summary>
+    public partial class HumanContour : UserControl
+    {
+        public HumanContour()
+        {
+            InitializeComponent();
+        }
+    }
+}
