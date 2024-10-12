397fc630d5f8bb9f7104b3bd3c07fb972536e7d0
k-hara <k.hara.pg@gmail.com>
2024-10-12 14:36:40 +0900

    +1

---
+++ FlexID.Calc.Tests/IndexReadTests.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlexID.Calc.Tests
+{
+    [TestClass]
+    public class IndexReadTests
+    {
+        [TestMethod]
+        public void TestReadNDX()
+        {
+            //var expectFile = TestFiles.Combine("SAFRead", "Ca-45_RadExpected.txt");
+            //var expectLines = File.ReadAllLines(expectFile);
+
+            var actuals = IndexDataReader.ReadNDX();
+
+            //CollectionAssert.AreEqual(expectLines, actualLines);
+
+        }
+    }
+}
