86f20d0decc64fdd273958033100555ac08f3dc6
k-hara <k.hara.pg@gmail.com>
2024-10-18 19:00:13 +0900

    +(stash)

--- FlexID.Calc/Expression.cs
+++ FlexID.Calc/Expression.cs
@@ -1,9 +1,30 @@
+using Sprache;
+using System.Globalization;
+
 namespace FlexID.Calc
 {
+    public class Expr
+    {
+    }
+
     public class LiteralValue
     {
         public string Expr;
 
         public decimal Value;
+    }
+
+    public static class Expression
+    {
+        private static Parser<string> IdentifierParser =
+            Parse.Identifier(Parse.Letter, Parse.LetterOrDigit.Or(Parse.Char('_')));
+
+        public LiteralValue Calc(string expr)
+        {
+            if (decimal.TryParse(expr, NumberStyles.Float, null, out decimal v))
+            {
+
+            }
+        }
     }
 }
