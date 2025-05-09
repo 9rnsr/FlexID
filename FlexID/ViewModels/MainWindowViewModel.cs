ac528ab1fce1d11eacf2853724bf0d63c6365b58
k-hara <k.hara.pg@gmail.com>
2025-05-09 16:07:46 +0900

    +1

--- FlexID/ViewModels/MainWindowViewModel.cs
+++ FlexID/ViewModels/MainWindowViewModel.cs
@@ -1,15 +1,22 @@
 using Prism.Mvvm;
 using Reactive.Bindings;
+using System.Windows;
 
 namespace FlexID.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
+        public ReactiveCommand AboutCommand { get; }
+
         /// <summary>
         /// コンストラクタ。
         /// </summary>
         public MainWindowViewModel()
         {
+            AboutCommand = new ReactiveCommand().WithSubscribe(() =>
+            {
+                MessageBox.Show("test", "About", MessageBoxButton.OK);
+            });
         }
     }
 
