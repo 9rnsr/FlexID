c8be33d6cc80c7e8102492bc8d3dce718f1a480a
k-hara <k.hara.pg@gmail.com>
2025-05-10 23:40:23 +0900

    feat: S係数データをインプット読み込み時に放射線データから毎回作成する

--- FlexID.Calc/InputDataReader_OIR.cs
+++ FlexID.Calc/InputDataReader_OIR.cs
@@ -10,6 +10,15 @@ namespace FlexID.Calc
     /// </summary>
     public class InputDataReader_OIR : InputDataReaderBase
     {
+        private static readonly SAFData safdataAM;
+        private static readonly SAFData safdataAF;
+
+        static InputDataReader_OIR()
+        {
+            safdataAM = SAFDataReader.ReadSAF(Sex.Male);
+            safdataAF = SAFDataReader.ReadSAF(Sex.Female);
+        }
+
         private readonly InputEvaluator evaluator = new InputEvaluator();
 
         private string inputTitle;
@@ -1129,93 +1138,78 @@ namespace FlexID.Calc
         /// <returns>キーが線源領域の名称、値が各標的領域に対する成人男女平均のS係数、となる辞書。</returns>
         private static Dictionary<string, double[]> ReadSCoeff(InputData data, NuclideData nuclide)
         {
+            if (safdataAM is null || safdataAF is null)
+                throw Program.Error($"Cannot read SAF data.");
+
             var nuc = nuclide.Name;
-            var fileAM = $"{nuc}_AM.txt";
-            var fileAF = $"{nuc}_AF.txt";
 
-            using (var readerAM = new StreamReader(Path.Combine("lib", "OIR", "Scoeff", fileAM)))
-            using (var readerAF = new StreamReader(Path.Combine("lib", "OIR", "Scoeff", fileAF)))
+            var calcScoeffAM = new CalcScoeff(safdataAM);
+            calcScoeffAM.InterpolationMethod = "PCHIP";
+            calcScoeffAM.CalcS(nuc);
+
+            var calcScoeffAF = new CalcScoeff(safdataAF);
+            calcScoeffAF.InterpolationMethod = "PCHIP";
+            calcScoeffAF.CalcS(nuc);
+
+            var sources = data.SourceRegions.Select(s => s.Name).ToArray();
+            var targets = data.TargetRegions;
+            var sourcesCount = sources.Length;
+            var targetsCount = targets.Length;
+
+            var table = sources.ToDictionary(s => s, s => new double[targetsCount]);
+
+            var columnOther = new double[targetsCount];
+            var indexOtherSources = nuclide.OtherSourceRegions.Select(s => Array.IndexOf(sources, s)).ToArray();
+
+            for (int indexT = 0; indexT < targetsCount; indexT++)
             {
-                var sources = data.SourceRegions.Select(s => s.Name).ToArray();
-                var targets = data.TargetRegions;
-                var sourcesCount = sources.Length;
-                var targetsCount = targets.Length;
+                double GetValueAM(int indexS) => calcScoeffAM.OutTotal[indexT + targetsCount * indexS];
+                double GetValueAF(int indexS) => calcScoeffAF.OutTotal[indexT + targetsCount * indexS];
 
-                // 1行目から線源領域の名称を配列で取得。
-                var sourcesAM = readerAM.ReadLine()?.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Skip(1).ToArray();
-                var sourcesAF = readerAF.ReadLine()?.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Skip(1).ToArray();
-                if (sourcesAM is null) throw Program.Error($"Incorrect S-Coefficient file format: {fileAM}");
-                if (sourcesAF is null) throw Program.Error($"Incorrect S-Coefficient file format: {fileAF}");
-                if (!Enumerable.SequenceEqual(sources, sourcesAM) || !Enumerable.SequenceEqual(sources, sourcesAF))
-                    throw Program.Error($"Found mismatch of source region names in S-Coefficient data for nuclide {nuc}.");
-
-                var table = sources.ToDictionary(s => s, s => new double[targetsCount]);
-
-                var columnOther = new double[targetsCount];
-                var indexOtherSources = nuclide.OtherSourceRegions.Select(s => Array.IndexOf(sources, s)).ToArray();
-
-                for (int indexT = 0; indexT < targetsCount; indexT++)
+                for (int indexS = 0; indexS < sourcesCount; indexS++)
                 {
-                    var columnsAM = readerAM.ReadLine()?.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
-                    var columnsAF = readerAF.ReadLine()?.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
-                    if (columnsAM?.Length != 1 + sourcesCount) throw Program.Error($"Incorrect S-Coefficient file format: {fileAM}");
-                    if (columnsAF?.Length != 1 + sourcesCount) throw Program.Error($"Incorrect S-Coefficient file format: {fileAF}");
-
-                    // 各行の1列目から標的領域の名称を取得。
-                    var targetAM = columnsAM[0];
-                    var targetAF = columnsAF[0];
-                    if (targets[indexT] != targetAM || targets[indexT] != targetAF)
-                        throw Program.Error($"Found mismatch of target region names in S-Coefficient data for nuclide {nuc}.");
-
-                    // 各線源領域からのS係数を取得する。
-                    var valuesAM = columnsAM.Skip(1).Select(v => double.Parse(v)).ToArray();
-                    var valuesAF = columnsAF.Skip(1).Select(v => double.Parse(v)).ToArray();
-
-                    for (int indexS = 0; indexS < sourcesCount; indexS++)
-                    {
-                        var sourceRegion = sources[indexS];
-                        var scoeffAM = valuesAM[indexS];
-                        var scoeffAF = valuesAF[indexS];
-
-                        // ここでS係数の男女平均を取る。
-                        var scoeff = (scoeffAM + scoeffAF) / 2;
-                        table[sourceRegion][indexT] = scoeff;
-                    }
-
-                    // 線源領域'Other'のS係数を計算する。
-                    var massOtherAM = 0.0;
-                    var massOtherAF = 0.0;
-                    var scoeffOtherAM = 0.0;
-                    var scoeffOtherAF = 0.0;
-                    foreach (var indexS in indexOtherSources)
-                    {
-                        var sourceRegion = sources[indexS];
-                        var massAM = data.SourceRegions[indexS].MaleMass;
-                        var massAF = data.SourceRegions[indexS].FemaleMass;
-                        var scoeffAM = valuesAM[indexS];
-                        var scoeffAF = valuesAF[indexS];
-
-                        massOtherAM += massAM;
-                        massOtherAF += massAF;
-                        scoeffOtherAM += massAM * scoeffAM;
-                        scoeffOtherAF += massAF * scoeffAF;
-                    }
-                    if (massOtherAM != 0) scoeffOtherAM /= massOtherAM;
-                    if (massOtherAF != 0) scoeffOtherAF /= massOtherAF;
+                    var sourceRegion = sources[indexS];
+                    var scoeffAM = GetValueAM(indexS);
+                    var scoeffAF = GetValueAF(indexS);
 
                     // ここでS係数の男女平均を取る。
-                    var scoeffOther = (scoeffOtherAM + scoeffOtherAF) / 2;
-                    columnOther[indexT] = scoeffOther;
+                    var scoeff = (scoeffAM + scoeffAF) / 2;
+                    table[sourceRegion][indexT] = scoeff;
                 }
 
-                // S係数データに'Other'列を追加する
-                table["Other"] = columnOther;
+                // 線源領域'Other'のS係数を計算する。
+                var massOtherAM = 0.0;
+                var massOtherAF = 0.0;
+                var scoeffOtherAM = 0.0;
+                var scoeffOtherAF = 0.0;
+                foreach (var indexS in indexOtherSources)
+                {
+                    var sourceRegion = sources[indexS];
+                    var massAM = data.SourceRegions[indexS].MaleMass;
+                    var massAF = data.SourceRegions[indexS].FemaleMass;
+                    var scoeffAM = GetValueAM(indexS);
+                    var scoeffAF = GetValueAF(indexS);
 
-                // 核種が考慮する線源領域の名称を設定する。
-                nuclide.SourceRegions = sources;
+                    massOtherAM += massAM;
+                    massOtherAF += massAF;
+                    scoeffOtherAM += massAM * scoeffAM;
+                    scoeffOtherAF += massAF * scoeffAF;
+                }
+                if (massOtherAM != 0) scoeffOtherAM /= massOtherAM;
+                if (massOtherAF != 0) scoeffOtherAF /= massOtherAF;
 
-                return table;
+                // ここでS係数の男女平均を取る。
+                var scoeffOther = (scoeffOtherAM + scoeffOtherAF) / 2;
+                columnOther[indexT] = scoeffOther;
             }
+
+            // S係数データに'Other'列を追加する
+            table["Other"] = columnOther;
+
+            // 核種が考慮する線源領域の名称を設定する。
+            nuclide.SourceRegions = sources;
+
+            return table;
         }
     }
 }
