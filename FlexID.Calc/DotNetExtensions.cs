cb47f3fac39e881d428d972821ff3828c8634024
k-hara <k.hara.pg@gmail.com>
2025-06-06 11:34:36 +0900

    feat: IReadOnlyList<T>に対するIndexOf拡張メソッドを追加

--- FlexID.Calc/DotNetExtensions.cs
+++ FlexID.Calc/DotNetExtensions.cs
@@ -2,6 +2,21 @@ namespace System.Collections.Generic
 {
     internal static class Extensions
     {
+        public static int IndexOf<T>(this IReadOnlyList<T> list, T element)
+        {
+            if (list is null)
+                throw new ArgumentNullException(nameof(list));
+
+            var comparer = EqualityComparer<T>.Default;
+            int count = list.Count;
+            for (int i = 0; i < count; i++)
+            {
+                if (comparer.Equals(list[i], element))
+                    return i;
+            }
+            return -1;
+        }
+
         public static V GetValueOrDefault<K, V>(this IReadOnlyDictionary<K, V> dictionary, K key)
         {
             if (dictionary.TryGetValue(key, out var value))
