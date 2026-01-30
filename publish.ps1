fb4b5262c53051020802e56c6e0e6a6e7926c320
k-hara <k.hara.pg@gmail.com>
2026-01-30 22:39:52 +0900

    chore: 発行物として実行ファイル一式のアーカイブを作成する

--- publish.ps1
+++ publish.ps1
@@ -1,8 +1,12 @@
-$SolutionDir = Join-Path -path $PSScriptRoot ""
-$PublishDir = Join-Path -path $SolutionDir "out/publish"
+$SolutionDir = Join-Path $PSScriptRoot ""
+$OutputDir   = Join-Path $SolutionDir "out"
+$PublishDir  = Join-Path $OutputDir "publish"
 
 # クリーンアップ
 dotnet msbuild -t:Clean -p:Configuration=Release
 
 # 発行
 dotnet publish -c Release -p:PublishDir=$PublishDir
+
+# アーカイブ
+Compress-Archive -Path $PublishDir/* -DestinationPath $OutputDir/FlexID.zip
