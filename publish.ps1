97ec654681f452ab8dbab9742c727af33d0e2823
k-hara <k.hara.pg@gmail.com>
2026-01-31 21:05:03 +0900

    feat: ランタイムを.NET 10.0に更新
    Costura.Fodyの代わりにPublishSingleFile=Trueを設定する。

--- publish.ps1
+++ publish.ps1
@@ -2,11 +2,27 @@ $SolutionDir = Join-Path $PSScriptRoot ""
 $OutputDir   = Join-Path $SolutionDir "out"
 $PublishDir  = Join-Path $OutputDir "publish"
 
+$BuildOpts = @(
+  "--configuration", "Release",
+  "-p:SolutionDir=$SolutionDir"
+)
+
 # クリーンアップ
-dotnet msbuild -t:Clean -p:Configuration=Release
+dotnet clean FlexID.Calc\FlexID.Calc.csproj     @BuildOpts
+dotnet clean FlexID\FlexID.csproj               @BuildOpts
+dotnet clean FlexID.Viewer\FlexID.Viewer.csproj @BuildOpts
+dotnet clean ResultChecker\ResultChecker.csproj @BuildOpts
+
+$PublishOpts = @(
+  "--self-contained",
+  "--output", "$PublishDir"
+)
 
 # 発行
-dotnet publish -c Release -p:PublishDir=$PublishDir
+dotnet publish FlexID.Calc\FlexID.Calc.csproj     @BuildOpts @PublishOpts
+dotnet publish FlexID\FlexID.csproj               @BuildOpts @PublishOpts
+dotnet publish FlexID.Viewer\FlexID.Viewer.csproj @BuildOpts @PublishOpts
+dotnet publish ResultChecker\ResultChecker.csproj @BuildOpts @PublishOpts
 
 # アーカイブ
-Compress-Archive -Path $PublishDir/* -DestinationPath $OutputDir/FlexID.zip
+Compress-Archive -Path $PublishDir/* -DestinationPath $OutputDir/FlexID.zip -Force
