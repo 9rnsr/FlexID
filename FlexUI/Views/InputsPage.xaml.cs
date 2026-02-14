44911824fa55e7fe0cb07779ea01637e9d46bebe
k-hara <k.hara.pg@gmail.com>
2026-02-14 20:50:56 +0900

    feat: OIRとEIRの組み込みインプット一覧についてDataTableで表示する

---
+++ FlexUI/Views/InputsPage.xaml.cs
@@ -0,0 +1,30 @@
+using FlexID.Models;
+using FlexID.ViewModels;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace FlexID.Views;
+
+public sealed partial class InputsPage : Page
+{
+    public InputsPage()
+    {
+        InitializeComponent();
+    }
+
+    public static readonly DependencyProperty TargetsProperty =
+        DependencyProperty.Register(
+            nameof(Targets), typeof(CheckableItemsView<InputTarget, InputTargetViewModel>), typeof(InputsPage),
+            new PropertyMetadata(null));
+
+    public CheckableItemsView<InputTarget, InputTargetViewModel>? Targets
+    {
+        get => (CheckableItemsView<InputTarget, InputTargetViewModel>?)GetValue(TargetsProperty);
+        set => SetValue(TargetsProperty, value);
+    }
+
+    private void ElementsFlyout_Closed(object sender, object e)
+    {
+        // TODO
+    }
+}
