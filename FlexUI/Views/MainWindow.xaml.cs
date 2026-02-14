4530e39d4cb42f3ef56341a339db58701c855ec6
k-hara <k.hara.pg@gmail.com>
2026-02-14 14:19:42 +0900

    feat: ウインドウサイズの制御のためにWinUIEx NuGetパッケージを導入

--- FlexUI/Views/MainWindow.xaml.cs
+++ FlexUI/Views/MainWindow.xaml.cs
@@ -7,19 +7,8 @@ public sealed partial class MainWindow
     public MainWindow(MainViewModel vm)
     {
         ViewModel = vm;
-        //SizeToContent = SizeToContent.Height;
 
         InitializeComponent();
-
-        //DataContext = ViewModel;
-
-        //ContentRendered += MainWindow_ContentRendered;
-
-        //void MainWindow_ContentRendered(object sender, System.EventArgs e)
-        //{
-        //    ContentRendered -= MainWindow_ContentRendered;
-        //    SizeToContent = SizeToContent.Manual;
-        //}
     }
 
     public MainViewModel ViewModel { get; }
