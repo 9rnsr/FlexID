8d8cea88c8562fceac631134aade3c76662bd8b8
k-hara <k.hara.pg@gmail.com>
2026-02-21 20:19:08 +0900

    feat: 計算対象となるインプットのViewModelクラスを拡張

--- FlexUI/ViewModels/InputTargetViewModel.cs
+++ FlexUI/ViewModels/InputTargetViewModel.cs
@@ -1,26 +1,68 @@
+using CommunityToolkit.Mvvm.ComponentModel;
 using FlexID.Models;
+using R3;
 
 namespace FlexID.ViewModels;
 
-public class InputTargetViewModel
+public partial class InputTargetViewModel : ViewModelBase, ICheckableItem
 {
-    /// <summary>
-    /// コンストラクタ
-    /// </summary>
-    /// <param name="target"></param>
     public InputTargetViewModel(InputTarget target)
     {
         InputTarget = target;
+
+        Progenies = [.. target.Progenies.Select(p => new ProgenySelectionViewModel(this, p))];
+
+        OnCheckedProgeniesChanged();
     }
 
     public InputTarget InputTarget { get; }
+
+    [ObservableProperty]
+    public partial bool IsChecked { get; set; }
+
+    string ICheckableItem.ItemText => Title;
 
     public string Title => InputTarget.Title;
 
     public string Nuclide => InputTarget.Nuclide;
 
-    /// <summary>
-    /// 子孫核種の計算モデルを持っている場合は<c>true</c>。
-    /// </summary>
-    public bool HasProgeny => InputTarget.Progenies.Count != 0;
+    public bool CanCheckProgenies => InputTarget.Progenies.Count != 0;
+
+    public IReadOnlyList<ProgenySelectionViewModel> Progenies { get; }
+
+    [ObservableProperty]
+    public partial string? CheckedProgenies { get; private set; }
+
+    void OnCheckedProgeniesChanged()
+    {
+        CheckedProgenies = string.Join(", ", Progenies.Where(p => p.IsChecked).Select(p => p.Progeny));
+    }
+
+    public partial class ProgenySelectionViewModelBase : ViewModelBase
+    {
+        private readonly InputTargetViewModel _owner;
+
+        public string Progeny { get; }
+
+        [ObservableProperty]
+        public partial bool IsChecked { get; set; } = true;
+
+        public ProgenySelectionViewModelBase(InputTargetViewModel owner, string progeny)
+        {
+            _owner = owner;
+            Progeny = progeny;
+        }
+
+        partial void OnIsCheckedChanged(bool value)
+        {
+            _owner.OnCheckedProgeniesChanged();
+        }
+    }
+}
+
+public partial class ProgenySelectionViewModel : InputTargetViewModel.ProgenySelectionViewModelBase
+{
+    public ProgenySelectionViewModel(InputTargetViewModel owner, string progeny)
+        : base(owner, progeny)
+    { }
 }
