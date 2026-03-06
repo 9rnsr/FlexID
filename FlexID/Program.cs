2a027f73ef2f3ab44162b67e438df5fb199d9de9
k-hara <k.hara.pg@gmail.com>
2026-03-07 00:07:37 +0900

    feat: コマンドラインプログラムの既定のカルチャをen-USに設定する

--- FlexID/Program.cs
+++ FlexID/Program.cs
@@ -1,4 +1,5 @@
 using System.CommandLine;
+using System.Globalization;
 
 namespace FlexID;
 
@@ -6,6 +7,9 @@ internal partial class Program
 {
     static async Task<int> Main(string[] args)
     {
+        // OSの言語設定を無視して、英語メッセージを表示する。
+        CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.GetCultureInfo("en-US");
+
         RootCommand rootCommand = new("CLI interface for FlexID (Flexible code for Internal Dosimetry)");
         rootCommand.Subcommands.Add(Program_Run.Command);
         rootCommand.Subcommands.Add(Program_Gen.Command);
