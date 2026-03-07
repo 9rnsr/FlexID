98959e69cab373a31d587da4a5ce0a3a9b7e3506
k-hara <k.hara.pg@gmail.com>
2026-03-07 13:20:34 +0900

    feat: 使用しているNuGetパッケージとそのライセンスのリストをJSONデータとして取得しバージョン情報画面の下部に表示する
    ローカルツールとしてnuget-licenseをインストールし、FlexUIのビルド時にJSONをPowerShellスクリプトlist-packages.ps1で生成する。

--- FlexUI/Views/AboutPage.xaml.cs
+++ FlexUI/Views/AboutPage.xaml.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using System.Text.Json;
 using Microsoft.UI.Xaml.Controls;
 
 namespace FlexID.Views;
@@ -14,6 +15,8 @@ public sealed partial class AboutPage : Page
         GitCommitId = ver.Split('+')[1];
         PointerSize = Environment.Is64BitProcess ? "64-bit" : "32-bit";
         VersionNumber = ver.Split('+')[0];
+
+        PackageList = LoadPackageList();
     }
 
     public string GitCommitId { get; }
@@ -32,4 +35,40 @@ public sealed partial class AboutPage : Page
 
     public string AESJ_Url = "https://www.aesj.net/en/";
     public string AESJ_RST_Url = "http://www.aesj.or.jp/~rst/";
+
+    public List<PackageInfo> PackageList { get; }
+
+    private List<PackageInfo> LoadPackageList()
+    {
+        var asm = Assembly.GetExecutingAssembly();
+        var resourceName = asm.GetManifestResourceNames()
+            .First(n => n.EndsWith("package-list.json"));
+
+        using var stream = asm.GetManifestResourceStream(resourceName)!;
+        using var reader = new StreamReader(stream);
+        var json = reader.ReadToEnd();
+
+        return JsonSerializer.Deserialize<List<PackageInfo>>(json)!;
+    }
+}
+
+public class PackageInfo
+{
+    public string PackageId { get; set; } = "";
+
+    public string PackageVersion { get; set; } = "";
+
+    public string PackageProjectUrl { get; set; } = "";
+
+    public string Copyright { get; set; } = "";
+
+    public string Authors { get; set; } = "";
+
+    public string Description { get; set; } = "";
+
+    public string License { get; set; } = "";
+
+    public string LicenseUrl { get; set; } = "";
+
+    public int LicenseInformationOrigin { get; set; }
 }
