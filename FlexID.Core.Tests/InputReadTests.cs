694e0992dd9ae9680f3ab8350bafb1b3cb86eee8
k-hara <k.hara.pg@gmail.com>
2026-04-01 21:46:11 +0900

    test: インプットの読み込みテストで、対応する期待値ファイルが存在することも確認する

--- FlexID.Core.Tests/InputReadTests.cs
+++ FlexID.Core.Tests/InputReadTests.cs
@@ -3,8 +3,22 @@ namespace FlexID;
 [TestClass]
 public class InputReadTests
 {
+    private static readonly string[] RetentionExpects;
+
+    static InputReadTests()
+    {
+        var expectDir = Path.Combine(AppResource.BaseDir, @"expect");
+        RetentionExpects = Directory.EnumerateFiles(expectDir, "*.dat", SearchOption.AllDirectories)
+            .Select(path =>
+            {
+                var nuclide = Path.GetFileName(Path.GetDirectoryName(path));
+                var datfile = Path.GetFileName(path);
+                return Path.Combine(nuclide, datfile);
+            }).ToArray();
+    }
+
     [TestMethod]
-    [DynamicData(nameof(GetTargets))]
+    [DynamicData(nameof(GetOirInuts))]
     public void Test_OIR(string inputPath)
     {
         var target = Path.GetFileNameWithoutExtension(inputPath);
@@ -12,9 +26,15 @@ public class InputReadTests
 
         var data = new InputDataReader_OIR(inputPath).Read();
         data.ShouldNotBeNull();
+
+        // インプットファイル名から取得した核種が、親核種として設定されているか確認。
+        data.Nuclides.First().Name.ShouldBe(nuclide);
+
+        var expectDat = Path.Combine(nuclide, target + ".dat");
+        RetentionExpects.ShouldContain(expectDat, $"Expect dat file {expectDat} not found for target {target}");
     }
 
-    public static IEnumerable<object[]> GetTargets()
+    public static IEnumerable<object[]> GetOirInuts()
     {
         var inputDir = Path.Combine(AppResource.BaseDir, @"inp\OIR");
         return Directory.EnumerateFiles(inputDir, "*.inp", SearchOption.AllDirectories)
@@ -22,13 +42,23 @@ public class InputReadTests
     }
 
     [TestMethod]
-    [DataRow("Sr-90_ing")]
-    public void Test_EIR(string target)
+    [DynamicData(nameof(GetEirInuts))]
+    public void Test_EIR(string inputPath)
     {
+        var target = Path.GetFileNameWithoutExtension(inputPath);
         var nuclide = target.Split('_')[0];
-        var inputPath = Path.Combine(AppResource.BaseDir, @"inp\EIR", nuclide, target + ".inp");
 
         var data = new InputDataReader_EIR(inputPath).Read();
         data.ShouldNotBeNull();
+
+        // インプットファイル名から取得した核種が、親核種として設定されているか確認。
+        data[0].Nuclides.First().Name.ShouldBe(nuclide);
+    }
+
+    public static IEnumerable<object[]> GetEirInuts()
+    {
+        var inputDir = Path.Combine(AppResource.BaseDir, @"inp\EIR");
+        return Directory.EnumerateFiles(inputDir, "*.inp", SearchOption.AllDirectories)
+            .Select(path => new object[] { path });
     }
 }
