e0d064edb9732a9b63ff6c79faf047bcf01baf06
k-hara <k.hara.pg@gmail.com>
2026-04-03 16:32:22 +0900

    chore: クリーンな状態から発行を実施する際にライセンス情報の取得に失敗する問題を修正
    licenses/配下に作成するThirdPartyNotices.txtやFlexUIに埋め込むライセンス情報（package-list.json)の生成のためにnuget-licenseを使用しているが、これがクリーンな状態からpublish.cmdを起動した場合に失敗するようになる問題が発生するようになった。
    
    問題としては、nuget-licenseによるパッケージの情報取得を行う前に対象となるNuGetパッケージの情報が適切に復元されていないことが原因だった。publish.ps1ではまずFlexID.csprjをビルドするが、この処理では当然ながらFlexUI.csproj側のNuGetパッケージは復元されず、しかしlist-pacages.ps1ではソリューション全体＝CLI＋GUIに対するライセンス情報を取得しようとするため、これが失敗していた。
    
    list-pacages.ps1において、ソリューション全体に対するdotnet restoreを行うことで問題を解消した。

--- scripts/list-packages.ps1
+++ scripts/list-packages.ps1
@@ -6,6 +6,8 @@ if ($args.Length -ne 2) {
 $SolutionFile = $args[0]
 $OutputJsonFile = $args[1]
 
+dotnet restore $SolutionFile
+
 dotnet tool run nuget-license `
   --input $SolutionFile `
   --ignored-packages "Microsoft.WindowsAppSDK;Microsoft.Windows.SDK.BuildTools;Microsoft.NET.ILLink.Tasks" `
